613e620a29303dcc076702ed2b3a15df4f147371
tranphuocloc <tranphuocloc070699@gmail.com>
2023-09-28 01:07:13 +0700

    setup for ec2

--- index.js
+++ index.js
@@ -12,21 +12,62 @@ app.use(express.urlencoded({ extended: true }));
 const accessKeyId = "AKIASE47AYPKFPAJJLHA";
 const secretAccessKey = "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";
 const region = "ca-central-1";
-const channelARN =
-  "arn:aws:kinesisvideo:ca-central-1:147972998100:channel/test123/1695785103159";
-const version = "xJbKmdb3SuXx6p2H4xqZ";
 
-const response = {
-  ChannelName: "test123",
-  ChannelARN: channelARN,
-  ChannelType: "SINGLE_MASTER",
-  ChannelStatus: "ACTIVE",
-  CreationTime: "September 27, 2023 10:25:03 AM",
-  SingleMasterConfiguration: {
-    MessageTtlSeconds: 60,
-  },
-  Version: version,
-  ResourceEndpointList: [
+// const response = {
+//   ChannelName: "test123",
+//   ChannelARN: channelARN,
+//   ChannelType: "SINGLE_MASTER",
+//   ChannelStatus: "ACTIVE",
+//   CreationTime: "September 27, 2023 10:25:03 AM",
+//   SingleMasterConfiguration: {
+//     MessageTtlSeconds: 60,
+//   },
+//   Version: version,
+//   ResourceEndpointList: [
+//     {
+//       Protocol: "HTTPS",
+//       ResourceEndpoint: "",
+//     },
+//     {
+//       Protocol: "WSS",
+//       ResourceEndpoint: "",
+//     },
+//   ],
+//   IceServerList: [
+//     {
+//       Uris: [],
+//       Username: "",
+//       Password: "",
+//       Ttl: 300,
+//     },
+//     {
+//       Uris: [],
+//       Username: "",
+//       Password: "",
+//       Ttl: 300,
+//     },
+//   ],
+//   PresignedWss: "",
+//   status_code: 200,
+// };
+
+app.get('/',async (req,res) =>{
+  res.send('Kinesis Server')
+})
+
+app.post("/school-streaming/school/:school_id/start", async (req, res) => {
+  try {
+    let response={}
+
+
+    const school_id = req.params.school_id;
+
+   const channelInfo =  await createSignalingChannel(`school-${school_id}`);
+   if(channelInfo?.error) res.json({success:false,message:channelInfo.error})
+    console.log("master calling...");
+
+    response = {...channelInfo,
+    ResourceEndpointList: [
     {
       Protocol: "HTTPS",
       ResourceEndpoint: "",
@@ -36,27 +77,8 @@ const response = {
       ResourceEndpoint: "",
     },
   ],
-  IceServerList: [
-    {
-      Uris: [],
-      Username: "",
-      Password: "",
-      Ttl: 300,
-    },
-    {
-      Uris: [],
-      Username: "",
-      Password: "",
-      Ttl: 300,
-    },
-  ],
-  PresignedWss: "",
-  status_code: 200,
-};
+    }
 
-app.post("/school-streaming/school/:school_id/start", async (req, res) => {
-  try {
-    const school_id = req.params.school_id;
     const kinesisVideoClient = new AWS.KinesisVideo({
       region,
       accessKeyId,
@@ -65,7 +87,7 @@ app.post("/school-streaming/school/:school_id/start", async (req, res) => {
     });
     const getSignalingChannelEndpointResponse = await kinesisVideoClient
       .getSignalingChannelEndpoint({
-        ChannelARN: channelARN,
+        ChannelARN: channelInfo.ChannelARN,
         SingleMasterChannelEndpointConfiguration: {
           Protocols: ["WSS", "HTTPS"],
           Role: Role.MASTER,
@@ -81,7 +103,7 @@ app.post("/school-streaming/school/:school_id/start", async (req, res) => {
         },
         {}
       );
-  
+
     response.ResourceEndpointList[0].ResourceEndpoint =
       endpointsByProtocol.HTTPS;
     response.ResourceEndpointList[1].ResourceEndpoint = endpointsByProtocol.WSS;
@@ -96,7 +118,7 @@ app.post("/school-streaming/school/:school_id/start", async (req, res) => {
       });
     const getIceServerConfigResponse = await kinesisVideoSignalingChannelsClient
       .getIceServerConfig({
-        ChannelARN: channelARN,
+        ChannelARN: channelInfo.ChannelARN,
       })
       .promise();
     const iceServers = [
@@ -111,12 +133,13 @@ app.post("/school-streaming/school/:school_id/start", async (req, res) => {
     );
     response.IceServerList = iceServers;
 
-    // console.log({iceServers})
+  
 
     res.json({
-      ...response
+      ...response,
     });
   } catch (error) {
+    console.log({error})
     res.json({
       success: false,
       message: error,
@@ -125,76 +148,214 @@ app.post("/school-streaming/school/:school_id/start", async (req, res) => {
 });
 
 app.post("/school-streaming/school/:school_id/view", async (req, res) => {
-    try {
-        console.log('view calling...')
-      const school_id = req.params.school_id;
-      const kinesisVideoClient = new AWS.KinesisVideo({
+  try {
+    let response={}
+
+
+    const school_id = req.params.school_id;
+
+   const channelInfo =  await createSignalingChannel(`school-${school_id}`);
+   if(channelInfo?.error) res.json({success:false,message:channelInfo.error})
+    console.log("viewer calling...");
+
+    response = {...channelInfo,
+    ResourceEndpointList: [
+    {
+      Protocol: "HTTPS",
+      ResourceEndpoint: "",
+    },
+    {
+      Protocol: "WSS",
+      ResourceEndpoint: "",
+    },
+  ],
+    }
+
+    const kinesisVideoClient = new AWS.KinesisVideo({
+      region,
+      accessKeyId,
+      secretAccessKey,
+      correctClockSkew: true,
+    });
+    const getSignalingChannelEndpointResponse = await kinesisVideoClient
+      .getSignalingChannelEndpoint({
+        ChannelARN: channelInfo.ChannelARN,
+        SingleMasterChannelEndpointConfiguration: {
+          Protocols: ["WSS", "HTTPS"],
+          Role: Role.VIEWER,
+        },
+      })
+      .promise();
+
+    const endpointsByProtocol =
+      getSignalingChannelEndpointResponse.ResourceEndpointList.reduce(
+        (endpoints, endpoint) => {
+          endpoints[endpoint.Protocol] = endpoint.ResourceEndpoint;
+          return endpoints;
+        },
+        {}
+      );
+
+    response.ResourceEndpointList[0].ResourceEndpoint =
+      endpointsByProtocol.HTTPS;
+    response.ResourceEndpointList[1].ResourceEndpoint = endpointsByProtocol.WSS;
+
+    const kinesisVideoSignalingChannelsClient =
+      new AWS.KinesisVideoSignalingChannels({
         region,
         accessKeyId,
         secretAccessKey,
+        endpoint: endpointsByProtocol.HTTPS,
         correctClockSkew: true,
       });
-      const getSignalingChannelEndpointResponse = await kinesisVideoClient
-        .getSignalingChannelEndpoint({
-          ChannelARN: channelARN,
-          SingleMasterChannelEndpointConfiguration: {
-            Protocols: ["WSS", "HTTPS"],
-            Role: Role.VIEWER,
-          },
-        })
-        .promise();
+    const getIceServerConfigResponse = await kinesisVideoSignalingChannelsClient
+      .getIceServerConfig({
+        ChannelARN: channelInfo.ChannelARN,
+      })
+      .promise();
+    const iceServers = [
+      { urls: `stun:stun.kinesisvideo.${region}.amazonaws.com:443` },
+    ];
+    getIceServerConfigResponse.IceServerList.forEach((iceServer) =>
+      iceServers.push({
+        urls: iceServer.Uris,
+        username: iceServer.Username,
+        credential: iceServer.Password,
+      })
+    );
+    response.IceServerList = iceServers;
+
   
-      const endpointsByProtocol =
-        getSignalingChannelEndpointResponse.ResourceEndpointList.reduce(
-          (endpoints, endpoint) => {
-            endpoints[endpoint.Protocol] = endpoint.ResourceEndpoint;
-            return endpoints;
-          },
-          {}
-        );
-     
-      response.ResourceEndpointList[0].ResourceEndpoint =
-        endpointsByProtocol.HTTPS;
-      response.ResourceEndpointList[1].ResourceEndpoint = endpointsByProtocol.WSS;
-  
-      const kinesisVideoSignalingChannelsClient =
-        new AWS.KinesisVideoSignalingChannels({
-          region,
-          accessKeyId,
-          secretAccessKey,
-          endpoint: endpointsByProtocol.HTTPS,
-          correctClockSkew: true,
-        });
-      const getIceServerConfigResponse = await kinesisVideoSignalingChannelsClient
-        .getIceServerConfig({
-          ChannelARN: channelARN,
-        })
-        .promise();
-      const iceServers = [
-        { urls: `stun:stun.kinesisvideo.${region}.amazonaws.com:443` },
-      ];
-      getIceServerConfigResponse.IceServerList.forEach((iceServer) =>
-        iceServers.push({
-          urls: iceServer.Uris,
-          username: iceServer.Username,
-          credential: iceServer.Password,
-        })
-      );
-      response.IceServerList = iceServers;
-  
-      // console.log({iceServers})
-  
-      res.json({
-        ...response
-      });
-    } catch (error) {
-      res.json({
-        success: false,
-        message: error,
-      });
-    }
-  });
+
+    res.json({
+      ...response,
+    });
+  } catch (error) {
+    console.log({error})
+    res.json({
+      success: false,
+      message: error,
+    });
+  }
+});
+
+
+// app.post("/school-streaming/school/:school_id/view", async (req, res) => {
+//   try {
+//     console.log("view calling...");
+//     const school_id = req.params.school_id;
+//     const kinesisVideoClient = new AWS.KinesisVideo({
+//       region,
+//       accessKeyId,
+//       secretAccessKey,
+//       correctClockSkew: true,
+//     });
+//     const getSignalingChannelEndpointResponse = await kinesisVideoClient
+//       .getSignalingChannelEndpoint({
+//         ChannelARN: channelARN,
+//         SingleMasterChannelEndpointConfiguration: {
+//           Protocols: ["WSS", "HTTPS"],
+//           Role: Role.VIEWER,
+//         },
+//       })
+//       .promise();
+
+//     const endpointsByProtocol =
+//       getSignalingChannelEndpointResponse.ResourceEndpointList.reduce(
+//         (endpoints, endpoint) => {
+//           endpoints[endpoint.Protocol] = endpoint.ResourceEndpoint;
+//           return endpoints;
+//         },
+//         {}
+//       );
+
+//     response.ResourceEndpointList[0].ResourceEndpoint =
+//       endpointsByProtocol.HTTPS;
+//     response.ResourceEndpointList[1].ResourceEndpoint = endpointsByProtocol.WSS;
+
+//     const kinesisVideoSignalingChannelsClient =
+//       new AWS.KinesisVideoSignalingChannels({
+//         region,
+//         accessKeyId,
+//         secretAccessKey,
+//         endpoint: endpointsByProtocol.HTTPS,
+//         correctClockSkew: true,
+//       });
+//     const getIceServerConfigResponse = await kinesisVideoSignalingChannelsClient
+//       .getIceServerConfig({
+//         ChannelARN: channelARN,
+//       })
+//       .promise();
+//     const iceServers = [
+//       { urls: `stun:stun.kinesisvideo.${region}.amazonaws.com:443` },
+//     ];
+//     getIceServerConfigResponse.IceServerList.forEach((iceServer) =>
+//       iceServers.push({
+//         urls: iceServer.Uris,
+//         username: iceServer.Username,
+//         credential: iceServer.Password,
+//       })
+//     );
+//     response.IceServerList = iceServers;
+
+//     // console.log({iceServers})
+
+//     res.json({
+//       ...response,
+//     });
+//   } catch (error) {
+//     res.json({
+//       success: false,
+//       message: error,
+//     });
+//   }
+// });
 
 app.listen(4444, () => {
   console.log("server listen at port 4444");
 });
+
+// Create singalingChanel
+/**
+ * This file demonstrates the process of creating a KVS Signaling Channel.
+ */
+async function createSignalingChannel(channelName) {
+  try {
+    console.log(
+      "[CREATE_SIGNALING_CHANNEL] Attempting to create signaling channel with name ",
+      channelName
+    );
+    // Create KVS client
+    const kinesisVideoClient = new AWS.KinesisVideo({
+      region: region,
+      accessKeyId: accessKeyId,
+      secretAccessKey: secretAccessKey,
+    });
+    const describeSignalingChannelResponse = await kinesisVideoClient
+      .describeSignalingChannel({
+        ChannelName: channelName,
+      })
+      .promise();
+    console.log({ describeSignalingChannelResponse });
+    if (describeSignalingChannelResponse?.ChannelInfo) {
+      return describeSignalingChannelResponse.ChannelInfo;
+    } else {
+      await kinesisVideoClient
+        .createSignalingChannel({
+          ChannelName: channelName,
+        })
+        .promise();
+
+      // Get signaling channel ARN
+      const getSignalingChannel = await kinesisVideoClient
+        .describeSignalingChannel({
+          ChannelName: channelName,
+        })
+        .promise();
+      return getSignalingChannel.ChannelInfo;
+    }
+  } catch (e) {
+    console.error("[CREATE_SIGNALING_CHANNEL] Encountered error:", e);
+    return e
+  }
+}
